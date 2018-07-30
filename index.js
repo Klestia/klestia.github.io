db67234c174a1565fcf9e56ea602b42769ccfafb
Klestia <41622343+Klestia@users.noreply.github.com>
2018-07-30 14:09:18 +0200

    Displayfix

--- index.js
+++ index.js
@@ -694,7 +694,7 @@
 
 		/* Set the width of the side navigation to 0 */
 	function closeNav() {
-		document.getElementById("mySidenav").style.transform = "translateX(-12px)";
+		document.getElementById("mySidenav").style.transform = "translateX(-200px)";
 	}
 	
 	function showSideMap()
