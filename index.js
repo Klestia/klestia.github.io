6beaa3b28e8579b2f992a048e5f6674f2cf199dd
Klestia <41622343+Klestia@users.noreply.github.com>
2018-07-30 14:13:44 +0200

    DisplayFix

--- index.js
+++ index.js
@@ -689,7 +689,7 @@
 		
 		
 	function openNav() {
-		document.getElementById("mySidenav").style.transform = "translateX(0)";
+		document.getElementById("mySidenav").style.transform = "translateX(0%)";
 	}
 
 		/* Set the width of the side navigation to 0 */
